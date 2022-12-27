e223e55a91cc8482be75e599feeaa711727d00d2
NATAN2014 <114419063+NATAN2014@users.noreply.github.com>
2022-12-27 16:23:11 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+var a = document.querySelector('.js');
+console.log(a);
+a.innerHTML = ":tiktok wbijajcie na UwUxercegowine i na srb_doge bnhkyof";
+var b = document.querySelector(".bt");
+console.log(b)
+b.onclick = function(){
+    a.innerHTML = "Zmiana";
+}
